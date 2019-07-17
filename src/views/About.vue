558624d0ce7c74f003ab67e453d8c720796c3141
Duang <hr837@163.com>
2019-07-17 22:21:31 +0800

    测试vue-baidu-map

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <baidu-map class="bm-view" ak="WXZF6FsTow7zlnw16ICbOyNPChQ67axg" :center="center" :zoom="zoom" :scroll-wheel-zoom="true"></baidu-map>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import BaiduMap from 'vue-baidu-map/components/map/Map.vue';
+@Component({
+  components: {
+    BaiduMap,
+  },
+})
+export default class Home extends Vue {
+
+  private center = { lng: 116.404, lat: 39.915 };
+  private zoom = 15;
+
+}
+</script>
+
+<style lang="less" scoped>
+.bm-view {
+  width: 100%;
+  height: 500px;
+}
+</style>
