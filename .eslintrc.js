9bfa468871e8bd9d92c2d3a6a8cb7d2a583528ad
黄瑞 <hr837@163.com>
2021-04-29 16:47:11 +0800

    :zap: (添加vue-lint)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,25 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    "prettier"
-  ],
-	globals:{
-    document: true,
-    localStorage: true,
-    window: true,
-    global:true,
-    module:true
+	root: true,
+	parser: "vue-eslint-parser",
+	parserOptions: {
+		parser: "@typescript-eslint/parser",
+		ecmaVersion: 2020,
+		sourceType: "module",
 	},
-	rules:{
-		"@typescript-eslint/ban-types": 'off'
-	}
+	plugins: ["@typescript-eslint"],
+	extends: [
+		"plugin:@typescript-eslint/recommended",
+		"plugin:vue/vue3-essential",
+		"prettier",
+	],
+	globals: {
+		document: true,
+		localStorage: true,
+		window: true,
+		global: true,
+		module: true,
+	},
+	rules: {
+		"@typescript-eslint/ban-types": "off",
+	},
 };
