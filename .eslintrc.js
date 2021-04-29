bacc115f48950f87ffe79fc07c327644829690f9
黄瑞 <hr837@163.com>
2021-04-29 17:52:12 +0800

    :zap:  lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,5 +26,6 @@ module.exports = {
 	rules: {
 		"@typescript-eslint/ban-types": "off",
 		"@typescript-eslint/explicit-module-boundary-types": "off",
+		"@typescript-eslint/no-unused-vars": "off",
 	},
 };
