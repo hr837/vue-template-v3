7faa759d39fb999f745a79f96180a759b7f5c74f
Duang(HuangRui)) <hr837@163.com>
2021-05-04 19:39:10 +0800

    :wrench:  修改commitlint hook

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,14 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const config = require("cz-emoji/lib/types.json");
+const types = config.map((type) => type.code);
+
 module.exports = {
 	extends: ["@commitlint/config-conventional"],
-	/*
-	 * Custom prompt configs
-	 */
-	prompt: {
-		messages: {},
-		questions: {
-			type: {
-				description: "please input type:",
-			},
+	parserPreset: {
+		parserOpts: {
+			headerPattern: /^(:\w*:)(?:\s)(?:\((.*?)\))?\s((?:.*(?=\())|.*)(?:\(#(\d*)\))?/,
+			headerCorrespondence: ["type", "scope", "subject", "ticket"],
 		},
+	},
+	rules: {
+		"type-enum": [2, "always", types],
 	},
 };
