6e69c256f2f345dc87cbea604f6517eb454d09b6
黄瑞 <huangrui@fintecher.cn>
2023-02-01 10:32:26 +0000

    修改main文件导入

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,15 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+// style
 import "./assets/styles/index.less";
-// 按需引入后部分样式会丢失 需手动导入
-import "element-plus/theme-chalk/el-loading.css";
-import "element-plus/theme-chalk/el-message.css";
-import "element-plus/theme-chalk/el-notification.css";
-import "element-plus/theme-chalk/el-message-box.css";
-import { createPinia } from "pinia";
-import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+import "@/config/element-plus.config";
+// store
+import store from "@/store";
+// http request 
 import useHttp from "@/config/http.config";
-const pinia = createPinia();
-pinia.use(piniaPluginPersistedstate);
-
 useHttp();
 
-const app = createApp(App).use(pinia).use(router);
+// app instance
+const app = createApp(App).use(store).use(router);
 app.mount("#app");
