93bca53b774f0878e3726eeb0e081a1e9cbdc08b
黄瑞 <huangrui@fintecher.cn>
2023-02-06 09:05:17 +0000

    fix:修改自动导入配置，只自动导入common目录下的vue组件

--- vue.config.mjs
+++ vue.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from "@vue/cli-service";
-import PluginAutoImport from "unplugin-auto-import/webpack";
-import PluginComponents from "unplugin-vue-components/webpack";
-import IconsResolver from "unplugin-icons/resolver";
-import PluginIconify from "unplugin-icons/webpack";
-import { FileSystemIconLoader } from "unplugin-icons/loaders";
-import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import { defineConfig } from '@vue/cli-service'
+import PluginAutoImport from 'unplugin-auto-import/webpack'
+import PluginComponents from 'unplugin-vue-components/webpack'
+import IconsResolver from 'unplugin-icons/resolver'
+import PluginIconify from 'unplugin-icons/webpack'
+import { FileSystemIconLoader } from 'unplugin-icons/loaders'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
   transpileDependencies: true,
@@ -12,19 +12,20 @@ export default defineConfig({
     plugins: [
       PluginAutoImport({
         resolvers: [ElementPlusResolver()],
-        dts: "typings/auto-imports.d.ts",
+        dts: 'typings/auto-imports.d.ts',
       }),
       PluginComponents({
-        dts: "typings/components.d.ts",
+        dts: 'typings/components.d.ts',
+        dirs: 'src/components/common',
         resolvers: [
           ElementPlusResolver(),
           IconsResolver({
-            prefix: "icon",
-            enabledCollections: ["icon-park-outline"],
-            customCollections: ["icon-local"],
+            prefix: 'icon',
+            enabledCollections: ['icon-park-outline'],
+            customCollections: ['icon-local'],
             alias: {
-              "park-outline": "icon-park-outline",
-              local: "icon-local",
+              'park-outline': 'icon-park-outline',
+              'local': 'icon-local',
             },
           }),
         ],
@@ -32,29 +33,29 @@ export default defineConfig({
       PluginIconify({
         autoInstall: true,
         customCollections: {
-          "icon-local": FileSystemIconLoader("./src/assets/icons"),
+          'icon-local': FileSystemIconLoader('./src/assets/icons'),
         },
         iconCustomizer(collection, icon, props) {
-          const iconClass = `iconify ${collection} ${collection}-${icon}`;
-          props.class = iconClass;
-          if (collection === "icon-local") {
-            props.width = "1.2em";
-            props.height = "1.2em";
+          const iconClass = `iconify ${collection} ${collection}-${icon}`
+          props.class = iconClass
+          if (collection === 'icon-local') {
+            props.width = '1.2em'
+            props.height = '1.2em'
           }
         },
       }),
     ],
   },
   css: {
-    sourceMap: process.env.NODE_ENV !== "production",
+    sourceMap: process.env.NODE_ENV !== 'production',
     loaderOptions: {
       less: {
         lessOptions: {
           globalVars: {
-            'color-primary': '#e30120'
-          }
-        }
-      }
-    }
+            'color-primary': '#e30120',
+          },
+        },
+      },
+    },
   },
-});
+})
